1a7c4e25a46c54e8751807c29a598542e7add07b
Kingblu33 <Demmarblue@gmail.com>
2022-10-13 19:11:41 -0400

    added contact functionality with emailjs

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Navbar from './components/Navbar/Navbar';
 import Project from './components/Projects/Project';
 import Footer from './components/Footer/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Contact from './components/Contact/Contact';
 function App() {
   return (
     <div className='container' >
       <Navbar />
       <Project />
+      <Contact />
       <Footer />
 
 
